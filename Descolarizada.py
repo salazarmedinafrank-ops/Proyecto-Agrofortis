4019b6e0ff504fe01ed35a8adc1af727350b035b
felipequiroga688-lgtm <felipequiroga688@gmail.com>
2026-09-08 16:58:51 -0500

    Update Descolarizada.py

--- Descolarizada.py
+++ Descolarizada.py
@@ -1,3 +1,28 @@
 No1, No2, No3 = map(int(input("Ingrese un numero"))) #El usuario ingresa 3 valores
 
 Numeromayor max(No1, No2, No3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+numero = input("ingrese un numero entero: ")
+
+try:
+    numero = int(numero)
+	print(El numero ingresado es entero:", numero)
+except ValueError:
+    print("Error: debe ingresar solamente un numero entero.")
