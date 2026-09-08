01b6943776764d899ed6670f6fb7ba51a536cb7b
salazarmedinafrank-ops <salazarmedinafrank@gmail.com>
2026-09-08 16:39:25 -0500

    Create Diferencia.py

---
+++ Diferencia.py
@@ -0,0 +1,10 @@
+No1 = int(input("Ingrese un numero")) #El usuario ingresa 3 valores
+No2 = int(input("Ingrese un numero"))
+No3 = int(input("Ingrese un numero"))
+
+
+if No1 >= No2 and a >= No3:
+    mayor = No1
+    if No2 >= No3:
+        intermedio, menor = No2, No3
+    else:
