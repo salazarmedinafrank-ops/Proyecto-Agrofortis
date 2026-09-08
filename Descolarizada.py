a4a4c423e4675566f3ecbd9e3913c41a9984d6ee
alexandramoralagos <alexandramoralagos@gmail.com>
2026-09-08 17:08:25 -0500

    Update Descolarizada.py

--- Descolarizada.py
+++ Descolarizada.py
@@ -2,7 +2,14 @@ No1, No2, No3 = map(int(input("Ingrese un numero"))) #El usuario ingresa 3 valor
 
 Numeromayor max(No1, No2, No3)
 
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
 
+if num1 == num2 and num2 == num3:
+    print("Los tres numeros son iguales")
+else:
+    print("Los numeros no son iguales")
 
 
 
