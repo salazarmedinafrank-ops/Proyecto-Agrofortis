762250b54bfc5cb0b002c87a5f3b789c8c724ae7
salazarmedinafrank-ops <salazarmedinafrank@gmail.com>
2026-09-08 16:18:51 -0500

    Create Descolarizada.py
    Mayor, menor y intermedio

---
+++ Descolarizada.py
@@ -0,0 +1,7 @@
+No1 = int(input("Ingrese un numero")) #El usuario ingresa 3 valores
+No2 = int(input("Ingrese un numero"))
+No3 = int(input("Ingrese un numero"))
+
+
+if No1 and No2 <= No3:
+    print("El numero mayor")
