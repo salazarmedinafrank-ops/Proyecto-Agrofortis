922338fa08dd318700b9c3cf78ab8a03987d0c66
salazarmedinafrank-ops <salazarmedinafrank@gmail.com>
2026-09-10 12:27:09 -0500

    Update Descolarizada.py

--- Descolarizada.py
+++ Descolarizada.py
@@ -1,9 +1,29 @@
-No1 = int(input("Ingrese un numero")) #El usuario ingresa 3 valores
-No2 = int(input("Ingrese un numero"))
-No3 = int(input("Ingrese un numero"))
+num1, num2, num3 = map(int, input("Ingrese 3 numeros: ").split())
 
+# Buscar el mayor
+if num1 >= num2 and num1 >= num3: 
+    mayor = num1
+elif num2 >= num1 and num2 >= num3:
+    mayor = num2
+else:
+    mayor = num3
 
-if No1 >= No2 and a >= No3:
-    mayor = No1
-    if No2 >= No3:
-        intermedio, menor = No2, No3
+# Buscar el menor
+if num1 <= num2 and num1 <= num3:
+    menor = num1
+elif num2 <= num1 and num2 <= num3:
+    menor = num2
+else:
+    menor = num3
+
+# Buscar el intermedio
+if num1 > num2 and num1 < num3:
+    Intermedio = num1
+elif num2 > num1 and num2 < num3:
+    Intermedio = num2
+else: 
+    Intermedio = num3
+
+print("El mayor es:", mayor)
+print("El menor es:", menor)
+print("El numero intermedio es:",Intermedio)
